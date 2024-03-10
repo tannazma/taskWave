82390cc9c9b3a2915131027dd481251c9c5de9d2
tannazma <97254921+tannazma@users.noreply.github.com>
2024-03-10 15:03:16 +0100

    create some default tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+// import AssignmentIcon from "@material-ui/icons/Assignment";
+// import DeleteIcon from "@material-ui/icons/Delete";
 
+const TaskWave = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Practice React Challenges",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Update CV",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Apply For Jobs",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      text: "Clean the house",
+      isCompleted: false,
+    },
+  ]);
 
-export default function Home() {
   return (
-    <>
-      <div>Hi</div>
-    </>
+    <div>
+      
+      {tasks.map((task) => (
+        <div key={task.id}>{task.text}</div>
+      ))}
+    </div>
   );
-}
+};
+export default TaskWave;
