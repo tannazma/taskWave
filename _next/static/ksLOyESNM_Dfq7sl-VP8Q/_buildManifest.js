489fa674345def9107c5fa26096f63704a77e3b9
tannazma <97254921+tannazma@users.noreply.github.com>
2024-03-13 21:48:04 +0100

    Updates

---
+++ _next/static/ksLOyESNM_Dfq7sl-VP8Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/684-8222be04ac1f6692.js","static/chunks/pages/index-7810b0450cd0c42e.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
